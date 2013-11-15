fa7858ec057f9c043c309831cffbbbaf82609a3a
gootra28 <gootra28@evergreen.edu>
2013-11-15 11:27:46 -0800

    Hw6 Added

---
+++ hw6.py
@@ -0,0 +1,86 @@
+# Name: Group 16: Hien Do, Travis Goodroad, Amber Hare
+# Evergreen Login: dohie11 , gootra28, hartra06
+# Computer Science Foundations
+# Programming as a Way of Life
+# Homework 6: create function : #Group pair Group 23 
+								#Group Member 1 Hannah Spencer 
+								#Group Member 2 Russ Arnold 
+
+# You may do your work by editing this file, or by typing code at the
+# command line and copying it into the appropriate part of this file when
+# you are done.  When you are done, running this file should compute and
+# print the answers to all the problems.
+
+
+###
+### Problem 3
+### Recognizing Types
+
+
+
+Good morning!
+['pink', 'yellow', 'green', 'red', 'purple', 'blue', 'orange']
+{'Karen': 22, 'Bob': 20, 'Lucy': 21, 'Adam': 21}
+[{'January': 1, 'December': 12}, {'January 1st': 'New Years', 'December 25th': 'Christmas'}]
+
+
+# This is just create a list in dictionary
+
+###
+### Problem 4
+### Identifying Values With an Index
+
+ 
+ antelope penguin bear
+ 
+# for problem for the index values starts at zoo(index value).
+# for zoo2 its not possible because zoo2 has only 1 item in the list therefore index is only len = 0
+
+
+
+
+###
+### Problem 5
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 5 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+
+###
+### Problem 6: Using a For Loop With a Dictionary
+### code fixed
+drinks = {"redbull": 3, "water": 4, "fresca": 1, "pom tea": 1, "rockstar": 2}
+
+for i in drinks:
+    print i
+
+# Answers: For this problem 6, we just use the "for loop" to pull items out 
+water
+fresca
+rockstar
+redbull
+pom tea
+
+
+
+###
+### Problem 7: Understanding and Using Assert
+###
+
+# This problem use to verify the columns or rows match or not 
+# For example:
+
+assert 1 == 0
+# this does not verify that it matches. Therefore this program will stop right here.
+
+assert x == x
+
+#this verify that it match therefore the program will continue to excute other codes.
+
+
+
+###
+### Collaboration: Travis Goodroad, Amber Hare. ( Group 16)
+###
